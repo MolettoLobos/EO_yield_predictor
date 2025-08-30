889e5ceceb5b8d63b2cbcab3d8cae6955caa676d
MolettoLobos <50592195+MolettoLobos@users.noreply.github.com>
2025-08-30 17:35:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 GDD-based multiband linear models for yield prediction (generic, 1..∞ bands)
 -----------------------------------------------------------------------------
-Author: Italo (helper module by ChatGPT)
+Author: Italo
 
 This module lets you:
   1) Feed an EO data cube shaped as (T, B, Y, X) where T indexes GDD (or time),
